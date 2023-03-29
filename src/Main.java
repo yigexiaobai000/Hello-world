745d2f6595cdd5d1f66cd2dca486331ad1306fe9
zhupengkai <test@zhupengkai.com>
2023-03-29 16:27:15 +0800

    import java.util.Scanner; public class Main {     public static void main(String[] args) {         Scanner in =new Scanner(System.in);         System.out.println("请输入被除数");         String a= in.nextLine();         System.out.println("请输入除数");         String b = in.nextLine();         System.out.println("请输入需要几个符合条件的结果");         String c = in.nextLine();
            int a1 = Integer.parseInt(a);
            int b1 = Integer.parseInt(b);
            int c1 = Integer.parseInt(c);
    
            int divided = a1;
            int divisor = b1;
            int found = c1;
            int d = 0;
            for (int i = 0;i<100 && d<found;i++){
            if (divided % divisor == 0){
                System.out.println(divided+"可以整除"+divisor+",的商是"+(divided/divisor));
                d++;
            }
            divided++;
            }
        }
    }

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,45 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Scanner in =new Scanner(System.in);
-        System.out.println("请输入被除数");
-        String a= in.nextLine();
-        System.out.println("请输入除数");
-        String b = in.nextLine();
-        System.out.println("请输入需要几个符合条件的结果");
-        String c = in.nextLine();
-
+        Scanner in = new Scanner(System.in);
+        System.out.println("请输入数字1-9之间的任意数字，获得其所对应的中文大写汉字");
+        String a = in.nextLine();
         int a1 = Integer.parseInt(a);
-        int b1 = Integer.parseInt(b);
-        int c1 = Integer.parseInt(c);
 
-        int divided = a1;
-        int divisor = b1;
-        int found = c1;
-        int d = 0;
-        for (int i = 0;i<100 && d<found;i++){
-        if (divided % divisor == 0){
-            System.out.println(divided+"可以整除"+divisor+",的商是"+(divided/divisor));
-            d++;
+        int n = a1;
+        String srt = n + "对应的中文数字是：";
+        switch (n) {
+            case 1:
+                srt += "壹";
+                break;
+            case 2:
+                srt += "贰";
+                break;
+            case 3:
+                srt += "叁";
+                break;
+            case 4:
+                srt += "肆";
+                break;
+            case 5:
+                srt += "伍";
+                break;
+            case 6:
+                srt += "陆";
+                break;
+            case 7:
+                srt += "柒";
+                break;
+            case 8:
+                srt += "捌";
+                break;
+            case 9:
+                srt += "玖";
+                break;
+            default:
+                System.out.println("错误的值" + n + ".值需要大于等于1，小于等于9。");
         }
-        divided++;
-        }
+        System.out.println(srt);
     }
 }
